c2b893b8f9ad2bbbd5e9b2b1cc95f5f9c10977a4
McCreeNing <ningyi0401@foxmail.com>
2018-07-06 14:28:12 +0800

    New repository workspace

---
+++ cmake-build-debug/CMakeFiles/ISP_DESIGN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ISP_DESIGN.dir/main.c.o"
+  "ISP_DESIGN.pdb"
+  "ISP_DESIGN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ISP_DESIGN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
