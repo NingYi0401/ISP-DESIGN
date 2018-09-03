89b3cf6aa08222b487c98e223830325dca33c02e
McCreeNing <ningyi0401@foxmail.com>
2018-09-03 15:51:54 +0800

    Use gcc + makefile to develop

--- step1.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by mccreening on 18-7-9.
-//
-#include "main.h"
-#include <iostream>
-void step1()
-{
-    printf("step1\n");
-}
--- step2/step2.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by mccreening on 18-7-9.
-//
-
-#ifndef TOP_STEP2_H
-#define TOP_STEP2_H
-
-void step3();
-#endif //TOP_STEP2_H
